d649e61f9bad1f0f565b2ee4eac31a4e40ef310d
mikesorae <lovepoko247@gmail.com>
2016-11-04 01:14:39 +0900

    add settings and sample files for frontend with webpack+react

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './front/src/js/main.jsx',
+  output: { path: path.join(__dirname, 'public/js'), filename: 'main.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
