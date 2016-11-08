93b2e06d59e8faf09ecbd7721a10118fae280643
mikesorae <lovepoko247@gmail.com>
2016-11-08 10:22:37 +0900

    create add new place component

--- front/src/js/components/place_input.jsx
+++ front/src/js/components/place_input.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 export default class PlaceInput extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <input type="text" />
+      <form className="row" onSubmit={this.onSubmit.bind(this)}>
+        <div className="col s10">
+          <input type="text" ref="newPlace" />
+        </div>
+        <div className="col s2">
+          <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+          </button>
+        </div>
+      </form>
     )
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    let input = this.refs.newPlace;
+    let newPlace  = input.value;
+    this.props.addEvent({ newPlace });
+    input.value = '';
   }
 }
--- front/src/js/main.jsx
+++ front/src/js/main.jsx
@@ -5,13 +5,35 @@ import NavBar from 'components/nav_bar';
 import PlaceList from 'components/place_list';
 import PlaceInput from 'components/place_input';
 
-let places = ["sample1", "sample2", "sample3"];
+let allItems = ["sample1", "sample2", "sample3"];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allItems: allItems
+    }
+    this.addEvent = this.addEvent.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <PlaceInput addEvent={ this.addEvent } />
+        <PlaceList places={ this.state.allItems } />
+      </div>
+    )
+  }
+
+  addEvent(todoItem) {
+    console.log(todoItem);
+    allItems.push(todoItem.newPlace);
+    this.setState({ allItems });
+  }
+}
 
 render(
-  <div>
-    <NavBar />
-    <PlaceInput />
-    <PlaceList places={ places }  />
-  </div>,
+  <App></App>,
   document.getElementById('app')
 );
